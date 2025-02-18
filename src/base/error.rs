9837b38a38a0b29992bb2aee2a9dc74cb4be9d09
韶光 <admin@gloryouth.com>
2025-02-18 23:49:03 +0800

    add todo

--- src/base/error.rs
+++ src/base/error.rs
@@ -109,7 +109,7 @@ impl std::fmt::Display for PingError {
 
 pub enum SharedError {
     Timeout,
-    Unreachable,
+    Unreachable, // todo linux unreachable
 }
 
 impl From<SharedError> for PingError {
